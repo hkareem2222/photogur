029497d5677b3a51435a7b57875f2cbd79d7389d
Husein Kareem <huseinkareem@gmail.com>
2014-06-18 15:21:48 -0400

    Refactroing to save new pictures in database

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -10,9 +10,22 @@ class PicturesController < ApplicationController
 
 
   def new
+    @picture = Picture.new
   end
 
   def create
-  	render :text => "Saving a picture. URL: #{params[:url]}, Title: #{params[:title]}, Artist: #{params[:artist]}"
+  	@picture = Picture.new(picture_params)
+     if @picture.save
+      # if the save for the picture was successful, go to index.html.erb
+      redirect_to pictures_url
+    else
+      render :new
+    end
   end
+
+  private 
+  def picture_params
+    params.require(:picture).permit(:artist, :title, :url)
+  end
+  
 end
